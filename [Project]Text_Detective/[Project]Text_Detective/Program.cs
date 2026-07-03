36fde4288b1474f8b0b6346048d3018094d8dea6
Jinnnyjin <jinnnny.jin@gmail.com>
2026-07-03 17:44:04 +0900

    [Init] 메인 루프 뼈대 + 메뉴 출력 + 정수입력 검증 구현

---
+++ [Project]Text_Detective/[Project]Text_Detective/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _Project_Text_Detective
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                SceneMain();
+                GetIntInput(1, BehaveKor.Length);
+            }
+        }
+
+        enum Behavior
+        {
+            Move, Search, Exercise, Study
+        }
+
+        static string[] BehaveKor = { "이동", "조사", "운동", "공부" };
+        //===================================
+        // 보기(집)
+        public static void SceneMain()
+        {
+            // 추후 집/집이외의 장소마다 선택지 다르게 부여
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($" [{i+1}] {BehaveKor[i]}");
+            }
+        }
+
+        //===================================
+        // 함수 - 정수 입력받기
+
+        public static int GetIntInput(int min, int max)
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int input))
+                {
+                    if (input >= min && input <= max)
+                    { return input; }
+                }
+                Console.WriteLine("\n잘못 입력했습니다. 보기 내 숫자만 입력가능");
+            }
+        }
+
+
+    }
+}
