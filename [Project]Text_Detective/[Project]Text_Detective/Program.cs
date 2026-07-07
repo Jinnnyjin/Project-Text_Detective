837dd106ffa0fcc46c8f6fc1a127859bbf9020dc
Jinnnyjin <jinnnny.jin@gmail.com>
2026-07-07 17:55:49 +0900

    [feat] 추리 배틀 시스템 구현 - 용의자 지목, 반박, 핵심 증거 판정

--- [Project]Text_Detective/[Project]Text_Detective/Program.cs
+++ [Project]Text_Detective/[Project]Text_Detective/Program.cs
@@ -4,8 +4,8 @@ using System.Threading.Channels;
 
 
 /*
-TODO :
-80% 체크 → 법정 배틀
+TODO : deduce 구현
+장소마다 조사 다르게 구현
  */
 namespace _Project_Text_Detective
 {
@@ -36,13 +36,13 @@ namespace _Project_Text_Detective
                 SceneMain(player);
                 List<Behavior> behaviors = GetBehavior(player);
                 int choice = GetIntInput(0, behaviors.Count);
-                if(choice == 0)
+                if (choice == 0)
                 {
                     isRunning = false;
                     Console.WriteLine("게임을 종료합니다.");
                     continue;
-                }    
-                
+                }
+
                 switch (behaviors[choice - 1])
                 {
                     case Behavior.Move:
@@ -61,7 +61,7 @@ namespace _Project_Text_Detective
                         OpenDiary(player);
                         break;
                     case Behavior.Deduce:
-                        //구현예정
+                        StartDeduce(player);
                         break;
                 }
                 Console.WriteLine("아무키나 누르세요");
@@ -69,24 +69,122 @@ namespace _Project_Text_Detective
             }
         }
         //===================================
+        // 함수 - 추리
+        public static void StartDeduce(Player player)
+        {
+            // 캐릭터 중 용의자 추리기 ( 피해자 1명 빼기 )
+            List<Character> suspects = new List<Character>();
+            for (int i = 0; i < CharacterData.TutoCharacters.Count; i++)
+            {
+                if (CharacterData.TutoCharacters[i].Type != CharacterType.Victim)
+                { suspects.Add(CharacterData.TutoCharacters[i]); }
+            }
+
+
+            // 용의자 입력받기
+            Console.WriteLine("범인으로 추정되는 용의자를 지목하세요");
+
+            for (int i = 0; i < suspects.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {suspects[i].Name}");
+            }
+
+            int choice = GetIntInput(1, suspects.Count);
+
+            // 범인 선택 시 추리 시작
+            if (suspects[choice - 1].Type == CharacterType.Criminal) Deduce(player);
+            // 범인 선택 실패
+            else FailCatch("범인");
+        }
+        //===================================
+        // 함수 - 용의자 검거 성공 시 추리
+        public static void Deduce(Player player)
+        {
+            Random random = new Random();
+            List<Debate> debate = new List<Debate>();
+
+            // 5가지 중 진술 3가지 중복없이 선택
+            while (debate.Count <= 2)
+            {
+                int debateNum = random.Next(0, DebateData.TutoDebate.Count);
+                if (debate.Contains(DebateData.TutoDebate[debateNum])) continue;
+                debate.Add(DebateData.TutoDebate[debateNum]);
+            }
+
+            // 진술에 대한 반박 성공여부
+            for (int i = 0; i < debate.Count; i++)
+            {
+                Console.WriteLine(debate[i].Statement);
+                OpenDiary(player);
+                Console.WriteLine("해당 진술에 대해 반박할 만한 증거를 선택하세요");
+                int choice = GetIntInput(1, player.Clues.Count);
+
+                if (player.Clues[choice-1] == debate[i].RightClue)
+                {
+                    Console.WriteLine("제대로 반박한것 같다.");
+                }
+                else
+                {
+                    FailCatch("알맞은 반박");
+                    return;
+                }
+            }
+            
+            // 핵심 증거 수집 확인
+            List<Clue> criticalClues = new List<Clue>();
+
+            // 핵심 증거 분류
+            for (int i = 0; i < ClueData.Tutoclues.Count; i++)
+            {
+                if (ClueData.Tutoclues[i].Importance == ClueImportance.Critical)
+                {
+                    criticalClues.Add(ClueData.Tutoclues[i]);
+                }
+            } 
+
+            for(int i = 0; i < criticalClues.Count; i++)
+            {
+                if (!player.Clues.Contains(criticalClues[i]))
+                {
+                    Console.WriteLine("중요한 증거를 아직 다 못 찾은 듯 하다.. 조금만 더 둘러보자");
+                    return;
+                }
+                
+            }
+            Console.WriteLine("범인을 찾았다! 이번 사건도 해결 완료!");
+
+        }
+        //===================================
+        // 함수 - 용의자 검거 실패
+        public static void FailCatch(string reason)
+        {
+
+            Console.WriteLine($"{reason}이 아닌 것 같다..");
+            Console.WriteLine("다시 생각해보자");
+
+            // 실패 패널티 턴+5
+            turnCount += 5;
+        }
+
+        //===================================
         // 함수 - 이동
         public static void Move(Player player)
         {
             Location[] movableLoca = GameRules.locaToLoca[player.Location];
-            
+
             Console.WriteLine("어디로 이동하겠습니까?");
             Console.WriteLine("[0] 취소");
 
-            for (int i = 0; i < movableLoca.Length ; i++)
+            for (int i = 0; i < movableLoca.Length; i++)
             {
-                Console.WriteLine($"[{i+1}] {LocationKor[(int)movableLoca[i]]}");
+                Console.WriteLine($"[{i + 1}] {LocationKor[(int)movableLoca[i]]}");
             }
 
             // 0 따로 취소처리
             int choice = GetIntInput(0, movableLoca.Length);
-                if (choice == 0) return;
+            if (choice == 0) return;
 
-            switch (movableLoca[choice -1])
+            switch (movableLoca[choice - 1])
             {
                 case Location.Cafe:
                     Console.WriteLine("카페로 이동합니다.");
@@ -119,16 +217,19 @@ namespace _Project_Text_Detective
         // 함수 - 조사
         public static void Investigate(Player player)
         {
+            //모두 모았다면
             if (player.Clues.Count == ClueCount)
             {
                 Console.WriteLine("이만하면 다 둘러본 모양이다. 여기서는 더이상 증거를 찾을 수 없을 것 같다.");
                 return;
             }
-            
+
             Random random = new Random();
 
+            // 얻었는지 확인용 변수
             int origin = player.Clues.Count;
 
+            // 증거 등급별 분할
             List<Clue> critical = new List<Clue>();
             List<Clue> minor = new List<Clue>();
 
@@ -143,18 +244,23 @@ namespace _Project_Text_Detective
                     minor.Add(ClueData.Tutoclues[i]);
                 }
             }
+
+            // 
             int clueNum = 0;
             while (player.Clues.Count == origin)
             {
                 int criticalPercentage = random.Next(0, 100);
-                if (criticalPercentage <= 20 + Math.Min(20,player.ObserveAbility))
+                // 최대 40%확률로 핵심 증거 획득
+                if (criticalPercentage <= 20 + Math.Min(20, player.ObserveAbility))
                 {
                     clueNum = random.Next(0, critical.Count);
+                    //이미 해당 증거를 가지고 있다면 패스
                     if (player.Clues.Contains(critical[clueNum])) continue;
 
                     player.Clues.Add(critical[clueNum]);
                     Console.WriteLine($"{critical[clueNum].Name}을 획득했습니다.");
                 }
+                //나머지 최소 60%확률로 일반 증거 획득
                 else
                 {
                     clueNum = random.Next(0, minor.Count);
@@ -165,8 +271,8 @@ namespace _Project_Text_Detective
                 }
             }
 
-                player.Hp--;
-                turnCount++;
+            player.Hp--;
+            turnCount++;
         }
         //===================================
         // 함수 - 운동
@@ -196,8 +302,8 @@ namespace _Project_Text_Detective
             player.Hp--;
             turnCount++;
             Random random = new Random();
-            int subject = random.Next(1,4);
-            switch(subject)
+            int subject = random.Next(1, 4);
+            switch (subject)
             {
                 case 1:
                     player.DeductAbility += 0.5f;
@@ -220,7 +326,7 @@ namespace _Project_Text_Detective
             List<Behavior> behaviors = GetBehavior(player);
             for (int i = 0; i < behaviors.Count; i++)
             {
-                Console.WriteLine($"[{i+1}] {BehaveKor[(int)behaviors[i]]}");
+                Console.WriteLine($"[{i + 1}] {BehaveKor[(int)behaviors[i]]}");
             }
         }
         //===================================
@@ -230,12 +336,12 @@ namespace _Project_Text_Detective
             List<Behavior> newBehaviors = new List<Behavior>(GameRules.locaToBehave[player.Location]);
 
             bool has80percent = (float)player.Clues.Count / ClueData.Tutoclues.Count >= 0.8;
-            bool isAtCafe = (player.Location == Location.Cafe); 
-            if (has80percent && isAtCafe) 
+            bool isAtCafe = (player.Location == Location.Cafe);
+            if (has80percent && isAtCafe)
             {
                 newBehaviors.Add(Behavior.Deduce);
             }
-                return newBehaviors;
+            return newBehaviors;
         }
 
         //===================================
@@ -251,11 +357,11 @@ namespace _Project_Text_Detective
         public static void ShowDiary(Player player)
         {
             Console.WriteLine("====추리 수첩 일부====");
-            for(int i = 0; i < Math.Min(player.Clues.Count, (int)player.DeductAbility); i++)
+            for (int i = 0; i < Math.Min(player.Clues.Count, (int)player.DeductAbility); i++)
             {
-                if(player.JudegeAbility >= 10 && player.Clues[i].Importance == ClueImportance.Critical)
+                if (player.JudegeAbility >= 10 && player.Clues[i].Importance == ClueImportance.Critical)
                     Console.WriteLine($"증거[{i + 1}] {player.Clues[i].Name}          ★중요");
-                else Console.WriteLine($"증거[{i+1}] {player.Clues[i].Name}");
+                else Console.WriteLine($"증거[{i + 1}] {player.Clues[i].Name}");
 
                 Console.WriteLine($" - {player.Clues[i].Description}");
             }
